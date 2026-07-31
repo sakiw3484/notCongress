5a463f9eb30a10035bc0f04b15db4bf929a4e4d6
sakiw3484 <sakiyahwinston101@gmail.com>
2026-07-30 20:20:50 -0400

    designing and formatting bill card with info from congress gov api

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const http = require("http");
+const { title } = require("process");
+const { json } = require("stream/consumers");
 const API_KEY = process.env.CONGRESS_API_KEY;
 
 const server = http.createServer(async(req, res) => {
+    console.log("Request received:", req.url);
+
     const url = new URL(req.url, "http://localhost:3000");
 
     if(url.pathname === "/api/bill"){
@@ -11,16 +15,64 @@ const server = http.createServer(async(req, res) => {
         const type = url.searchParams.get("type");
         const number = url.searchParams.get("number");
         try{
-            const response = await fetch(
+            //get basic bill info
+            const billResponse = await fetch(
                 `https://api.congress.gov/v3/bill/${congress}/${type}/${number}?api_key=${API_KEY}`
             );
-            const data = await response.json();
+            const billData = await billResponse.json();
+
+
+            //get bill summary 
+            const summaryResponse = await fetch(
+                `https://api.congress.gov/v3/bill/${congress}/${type}/${number}/summaries?api_key=${API_KEY}`
+            );
+            const summaryData = await summaryResponse.json();
+
+            const summary = summaryData.summaries[0].text;
+            summary = summary.replace(/<p><strong>.*?<\/strong><\/p>/, "");
+            const shortSummary = summary.replace(/<[^>]*>/g, "") // Remove HTML tags
+            .replace(/\s+/g, " ")    // Remove extra whitespace
+            .trim()
+            .slice(0, 250) + "...";
+
+
+            if(!billData.bill) {
+                res.writeHead(404, {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                });
+                res.end(JSON.stringify({
+                    error: "Bill not found."
+                }));
+                return;
+            }
+            console.log("Received data");
+
+            const billsInfo ={
+                title: billData.bill.title,
+                type: billData.bill.type,
+                number: billData.bill.number,
+                policyArea: billData.bill.policyArea?.name,
+                originChamber: billData.bill.originChamber,
+                introducedDate: billData.bill.introducedDate,
+                sponsors: billData.bill.sponsors[0]?.fullName,
+                summary: shortSummary
+               
+            
+        
+            };
+            //console.log(billsInfo);
+           // console.log(data.bill.sponsors);
+            //console.log(summary);
+
+
             res.writeHead(200, {
                 "Content-Type": "application/json",
                 "Access-Control-Allow-Origin": "*"
             });
             
-            res.end(JSON.stringify(data));
+            res.end(JSON.stringify(billsInfo));
+            console.log(billsInfo);
 
         }catch (err){
             res.writeHead(500, {
